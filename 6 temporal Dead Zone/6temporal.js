afc903deb143111902f515a2b3d8dda43aae9582
Shani Patel <shanipatel37780@gmail.com>
2025-11-23 22:18:20 +0530

    temporal dead zone
    basic learning

---
+++ 6 temporal Dead Zone/6temporal.js
@@ -0,0 +1,30 @@
+// global scope
+// const age = 15;
+// console.log(age)
+// {
+//     console.log(age)
+// }
+// if(true){
+//     console.log(age)
+// }
+// for(let i=0; i<2; i++){
+//     console.log(age)
+// }
+// function seyHe(){
+//     console.log("hii",age)
+// }
+// seyHe();
+
+// function scope
+// function sayHe(){
+//     var name = "hello word";
+//     console.log("shani patel",name)
+// }
+// sayHe();
+
+// temporal dead zone
+console.log(marks);
+console.log("shani");
+console.log("patel");
+var marks = 100;
+console.log(marks);
