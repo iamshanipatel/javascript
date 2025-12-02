20fcedbfd8b498c3f923306e6370d5934bd8fc1b
Shani Patel <shanipatel37780@gmail.com>
2025-12-02 21:46:55 +0530

    Error handling
    basic learning

---
+++ 9 Error handling/Error.js
@@ -0,0 +1,40 @@
+// compile time error
+
+// syntax error
+// console.log(1;
+
+// runtime error
+// reference error
+// console.log();
+
+//  try{
+//      console.log("try block starts here");
+//      console.log(x); // refernce error
+//      console.log("Iam not error")
+//      console.log("try block end here")
+//  }
+//  catch(error){
+//      console.log("I AM INSIDE CATCH BLOCK")
+//      console.log("your error is here:", error)
+    
+//  } finally{
+//      console.log("I will ren everytime, as I am finally block")
+//  }
+
+// let create a custom error
+//  try{
+//      // Reference error
+//      console.log(x)
+//  }
+//  catch(error){
+//      throw new Error ("Bhai pehle declare kro, fer print karna")
+//  }
+
+//  let errorCode = 100;
+//  if(errorCode == 100){
+//      throw new Error("INvalid Json")
+//  }
+
+function test(){
+    alert("hello word")
+}
