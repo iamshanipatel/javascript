64608d3cc795c45d5b9873b412e2d9032dc386fc
Shani Patel <shanipatel37780@gmail.com>
2025-12-06 22:56:43 +0530

    DOM manipulationchanging HTML
    basic learning time

---
+++ 10DOMmanipulationchangingHTML/10DOM&manipulation.js
@@ -0,0 +1,29 @@
+ // console.log("shani")
+// window.console.log("shani patel")
+
+//  document.getElementById("fpara")
+// window.console.log(window)
+  //console.log(document.body)
+
+//  document.body.style.background = "yellow";
+
+// document.getElementsByTagName('p');
+// document.getElementsByTagName('#fpara')
+// document.querySelector('.textMatter')
+
+// const mybody = document.body;
+// mybody.style.backgroundColor = "red"
+// console.log(mybody);
+
+// console.log(document.body)
+
+// document.body.style.backgroundColor = "red";
+// console.log(document.getElementsByClassName("ttp2"))
+// document.getElementsByClassName("ttp2")[0].innerText = "werty";
+// document.getElementById("ssp").innerText = "shani"
+
+// console.log(document.querySelectorAll('p'))
+ 
+// let para = $0
+// console.log(para)
+
